fd54a23063fa0b06d1e128891d6cc3e83f1f8e02
klimanovakateryna <klimanovakateryna@gmail.com>
2024-06-22 00:31:04 +0300

    Add a function that reverses a word

--- Two-pointers/ReverseWords.java
+++ Two-pointers/ReverseWords.java
@@ -1,1 +1,22 @@
 
+public class ReverseWords {
+    public static String reverseWords(String sentence){
+        String[] arr = sentence.trim().split("\\s+");
+        int start = 0;
+        int end = arr.length - 1;
+        while(start < end){
+            String temp = arr[start];
+            arr[start] = arr[end];
+            arr[end] = temp;
+            start++;
+            end--;
+        }
+        return String.join(" ", arr); //join concatenates the elements of the array
+    }
+
+    public static void main(String[] args){
+        String testString = "MULTIPLE     SPACES";
+        String res = reverseWords(testString);
+        System.out.println(res);
+    }
+}
